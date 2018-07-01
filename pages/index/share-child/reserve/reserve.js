857ac3f53a68f905458737f1ca61d1b1007a69f3
阿斯玛 <asuma@asimadeMacBook-Pro.local>
2018-07-01 13:15:48 +0800

    tmp_update

--- pages/index/share-child/reserve/reserve.js
+++ pages/index/share-child/reserve/reserve.js
@@ -1,4 +1,13 @@
 const util = require('../../../../utils/util.js')
+const nowDate = new Date()
+const someDate = new Date(nowDate.getTime() + 2 * 60 * 60 * 1000);
+var startDate = util.dateNeed(nowDate)
+var endDate = util.dateNeed(someDate)
+const minus = (s, e) => {
+  var res = parseInt(e.getTime() - s.getTime()) / (60 * 60 * 1000)
+  return Math.round((res * 10)) / 10
+}
+
 Component({
   /**
    * 组件的属性列表
@@ -10,16 +19,43 @@ Component({
    * 组件的初始数据
    */
   data: {
-    swiper: { dots: true, active_color: '#FF464A' }
+    swiper: { dots: true, active_color: '#FF464A' },
+    initdate:{
+      limitS: startDate,
+      limitE: endDate,
+      start: startDate,
+      end: endDate,
+      distance: minus(nowDate, someDate)
+    }
   },
   ready: function (e) {
-    console.log(new Date())
-    console.log(util.formatTime(new Date()));
+    console.log(nowDate)
+    console.log(util.formatTime(nowDate));
+    console.log(util.dateNeed(nowDate));
   },
   /**
    * 组件的方法列表
    */
   methods: {
-    
+    bindStartDateChange:function(e){
+      let start = new Date(e.detail.value + ' ' + this.data.initdate.start[3])
+      let end = new Date(start.getTime() + 2 * 60 * 60 * 1000)
+
+      this.setData({
+        'initdate.start': util.dateNeed(start),
+        'initdate.end': util.dateNeed(end),
+        'initdate.distance': minus(start, end)
+      })
+    },
+    bindStartTimeChange:function(e){
+      let start = new Date(this.data.initdate.start[1] + ' ' + e.detail.value)
+      let end = new Date(this.data.initdate.end[1] + ' ' + this.data.initdate.end[3])
+
+      this.setData({
+        'initdate.start': util.dateNeed(start),
+        'initdate.end': util.dateNeed(end),
+        'initdate.distance': minus(start, end)
+      })
+    }
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,23 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const formatWeek = n => {
+  const weeks = ['周日', '周一', '周二', '周三', '周四', '周五', '周六'];
+  return weeks[n];
+}
+
+const dateNeed = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const week = date.getDay()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+
+  return [year, [month, day].map(formatNumber).join('-'), formatWeek(week),[hour, minute].map(formatNumber).join(':')];
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  dateNeed: dateNeed
 }
