77f3dffdb4dba875a5e613dd925930ad7e111b65
iAsuma <770878450@qq.com>
2018-07-27 17:33:58 +0800

    33

--- component/plate/plate.js
+++ component/plate/plate.js
@@ -1,4 +1,6 @@
 // component/plate/plate.js
+var words_len = 0
+var old_idx = 0
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +13,32 @@ Component({
    * 组件的初始数据
    */
   data: {
-    words: []
+    words: [],
+    active:[]
   },
   attached:function(){
-    var words = ['京', '津', '冀', '晋', '蒙', '辽', '吉', '黑', '沪', '苏', '浙', '皖', '闽', '赣', '鲁', '豫', '鄂', '湘', '粤', '桂', '琼', '渝', '川', '贵', '云', '藏', '陕', '甘', '青', '宁', '新'];
+    //var words = [{n:'jing', i: 'active'}]
+    var words = ['京', '津', '冀', '晋', '蒙', '辽', '吉', '黑', '沪', '苏', '浙', '皖', '闽', '赣', '鲁', '豫', '鄂', '湘', '粤', '桂', '琼', '渝', '川', '贵', '云', '藏', '陕', '甘', '青', '宁', '新','台'];
     this.setData({
       words: words
     })
+
+    words_len = words.length;
+    var active = []
+    for(var i =0;i<words_len;i++){
+      if(words[i] == '苏'){
+        active[i] = 'active'
+        old_idx = i;
+      }else{
+        active[i] = ''
+      }
+    }
+
+    this.setData({
+      active: active
+    })
+
+    console.log(active)
   },
   /**
    * 组件的方法列表
@@ -25,6 +46,35 @@ Component({
   methods: {
     selectCurrent:function(e){
       console.log(e)
+      
+      var curr_index = e.target.dataset.idx
+
+      this.setData({
+        ['active[' + old_idx + ']']: ''
+      })
+
+      old_idx = curr_index
+      
+      this.setData({
+        ['active[' + curr_index + ']']: 'active'
+      })
+      
+      console.log(curr_index)
+
+      // for (var i = 0; i < words_len; i++) {
+      //   if (i == curr_index) {
+      //     this.setData({
+      //       ['active[' + i + ']']: 'active'
+      //     })
+      //   } else {
+      //     this.setData({
+      //       ['active[' + i + ']']: ''
+      //     })
+      //   }
+      // }
+
+      
+      console.log(this.data.active)
     }
   }
 })
