add13b468d439e5eeb225e9077615d1d8943a0ac
iAsuma <770878450@qq.com>
2018-08-30 13:27:04 +0800

    debug

---
+++ component/keyboard/keyboard.js
@@ -0,0 +1,26 @@
+// component/keyboard/keyboard.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    keyNumber: '1234567890',
+    keyEnInput1: 'QWERTYUIOP',
+    keyEnInput2: 'ASDFGHJKL',
+    keyEnInput3: 'ZXCVBNM'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
